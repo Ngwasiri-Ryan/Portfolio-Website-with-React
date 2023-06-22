dbf39b634ddaa9ad294e3e329f815a3ca4415d22
Ngwasiri Ryan <ryanngwasiri@gmail.com>
2023-06-22 09:49:40 +0100

    updated about page

--- src/components/About/content.jsx
+++ src/components/About/content.jsx
@@ -7,17 +7,39 @@ function content() {
            <div className='heading'>
                <span>About</span> Me
            </div>
+           <div className="boxes">
+            <div className="box">
+
+            </div>
+            <div className="box">
+
+            </div>
+            <div className="box">
+
+            </div>
+           </div>
 
             <p>
                 Hello there!  I am an aspiring software engineer and
                 Fullstack Developer.
                 <br/>
-                <br/>
                  I am currently a software engineering student at the University of Buea.
+                 
+                 I can built frontend web applications for my clients  and meet up to 
+                 their needs for their businesses and their presentations.
+                 I am currently a software engineering student at the University of Buea.
+                 
                  <br/>
+                 <br/>
+                
+                 
+                 I can built frontend web applications for my clients  and meet up to 
+                 their needs for their businesses and their presentations.
+                 I am currently a software engineering student at the University of Buea.
                  <br/>
                  I can built frontend web applications for my clients  and meet up to 
                  their needs for their businesses and their presentations.
+
             </p>
                <center><button>Let's Talk</button></center>
         </div>
